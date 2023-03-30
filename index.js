ddd0650ab3f8bed2987637a50580b1926bff318e
freda <ndyfreda@gmail.com>
2023-03-30 14:50:13 -0400

    arrow funtions

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide (){
+    let a = 2000
+    let b = 100
+    let result = a/b;
+    return result;
+}
+
+const square = () => {
+    let x = 2;
+    return x*x;
+}
+
+const add = () => {
+    let a = 3;
+    let b = 4;
+    return a + b;
+}
